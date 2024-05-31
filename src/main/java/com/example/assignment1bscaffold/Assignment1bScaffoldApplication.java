c928832439cb311272c51546b282a16f00753066
Rishhi <98315242+rishhi-patel@users.noreply.github.com>
2024-05-31 17:04:26 -0400

    test  code

--- src/main/java/com/example/assignment1bscaffold/Assignment1bScaffoldApplication.java
+++ src/main/java/com/example/assignment1bscaffold/Assignment1bScaffoldApplication.java
@@ -2,15 +2,20 @@ package com.example.assignment1bscaffold;
 
 import java.sql.Date;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Assignment1bScaffoldApplication {
+public class Assignment1bScaffoldApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Assignment1bScaffoldApplication.class, args);
 
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
 		WeddingController controller = new WeddingController(
 				new VenueService(new VenueRepository()),
 				new GuestService(new GuestRepository()),
@@ -18,8 +23,8 @@ public class Assignment1bScaffoldApplication {
 
 		// Sample usage of wedding planner
 		controller.bookVenue("Lakeside Banquet", "2024-05-20");
-		controller.addGuest("Jane Doe", "jane.doe@example.com");
-		controller.recordExpense("Venue Booking", 2000);
+		controller.addGuest("Rishikumar Patel", "rishhi.dev@gmail.com");
+		controller.recordExpense("Venue Booking", 20000);
 		controller.printEventDetails();
 	}
 
