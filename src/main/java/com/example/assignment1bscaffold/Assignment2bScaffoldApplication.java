8b3a984bae79feb5dcf47b579cef759af2663735
Rishhi <98315242+rishhi-patel@users.noreply.github.com>
2024-06-14 16:54:57 -0400

    Update Assignment2bScaffoldApplication.java

--- src/main/java/com/example/assignment1bscaffold/Assignment2bScaffoldApplication.java
+++ src/main/java/com/example/assignment1bscaffold/Assignment2bScaffoldApplication.java
@@ -1,13 +1,19 @@
 package com.example.assignment1bscaffold;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Assignment2bScaffoldApplication {
+public class Assignment2bScaffoldApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Assignment2bScaffoldApplication.class, args);
 
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Server Mybe online");
 	}
 }
