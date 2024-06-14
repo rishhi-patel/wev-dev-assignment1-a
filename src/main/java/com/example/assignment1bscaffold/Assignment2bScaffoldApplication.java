0e2a967c8e8169b14d686f4a70eb3ec808da67e4
Rishhi <98315242+rishhi-patel@users.noreply.github.com>
2024-06-14 16:51:23 -0400

    -- inint

--- src/main/java/com/example/assignment1bscaffold/Assignment2bScaffoldApplication.java
+++ src/main/java/com/example/assignment1bscaffold/Assignment2bScaffoldApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Assignment1bScaffoldApplication {
+public class Assignment2bScaffoldApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Assignment1bScaffoldApplication.class, args);
+		SpringApplication.run(Assignment2bScaffoldApplication.class, args);
 
 	}
 }
